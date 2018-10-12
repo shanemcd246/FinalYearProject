8015e77773039871a048c9b6963af9f3a6b6dc1b
shanemcd246 <42997755+shanemcd246@users.noreply.github.com>
2018-10-12 12:13:08 +0100

    Initial commit

---
+++ OnlyGreen.py
@@ -0,0 +1,27 @@
+import numpy as np
+import cv2
+
+
+cap = cv2.VideoCapture(0)
+sensitivity = 25
+while True:
+    _, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    #HSV hue sat value
+    lower_blue = np.array([90-sensitivity,100,50])
+    upper_blue = np.array([90+sensitivity,255,255])
+
+    mask = cv2.inRange(hsv, lower_blue,upper_blue)
+    res = cv2.bitwise_and(frame, frame, mask = mask)
+
+    cv2.imshow('frame', frame)
+    cv2.imshow('mask', mask)
+    cv2.imshow('res', res)
+
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+
+cv2.destroyAllWindows()
+cap.release()
