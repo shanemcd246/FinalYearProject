c1525aff4dd5a850c0a1f25b90a41d57c34cf25f
shanemcd246 <42997755+shanemcd246@users.noreply.github.com>
2019-04-28 03:07:31 +0100

    Update workingTracker.py

--- workingTracker.py
+++ workingTracker.py
@@ -1,230 +1,283 @@
-#   Shane Mc Donagh
-#   Smart Sprayer
-#   Final Year Project
-
-import cv2
-import imutils
-import numpy as np
+import socket
 import time
 import pickle
-import socket
 import threading
-import os
 from PIL import Image
 import io
-import sys
+import random
+import tensorflow as tf
+import numpy as np
+import mysql.connector
+import os
+import datetime
 
-global trackCount
-trackCount = 1
+
+TfModel = "c://ForDemo2//output_graph.pb"
+TfName = "c://ForDemo2//output_labels.txt"
+inputLayer = "Placeholder"
+outputLayer = "final_result"
+Ugraph = None
+doubleCheckEnabled = False
+
+dataToSend = []
+dataReceived = []
+alreadyDetected = []
+doubleCheck = []
 
 lock = threading.Lock()
 
-cap = cv2.VideoCapture(0)
-time.sleep(2.0)
-myDictList = []
-detectedList = []
-dataToSend = []
-dataReceived = []
 
-def make_1080p():
-    cap.set(3, 1920)
-    cap.set(4, 1080)
+def addToDatabase(name, score, image, detected, lon, lat):
+    mydb = mysql.connector.connect(host="127.0.0.1", user="root", passwd="root")
+    mycursor = mydb.cursor()
+    mycursor.execute("SHOW DATABASES")
+    dbList =[]
+    for x in mycursor:
+        dbList.append(str(x))
+    print("Adding "+name+" to Database....")
+    print("@@@@@@@@@@@@@@@@@@@@@")
+    if "('finalyearproject',)" not in dbList:
+        mycursor.execute("CREATE DATABASE finalYearProject")
+    if detected ==1:
+        today = datetime.date.today()
+        today = str(today.day) + "_" + str(today.month) + "_" + str(today.year)
+        mydb = mysql.connector.connect(host="127.0.0.1", user="root", passwd="root", database="finalYearProject")
+        mycursor = mydb.cursor()
+        sql = "INSERT INTO feildrun_"+today+" (PLANTNAME, SCORE, IMAGE, LON, LAT) VALUES (%s, %s, %s, %s, %s)"
+        val = (name, str(score), image, lon, lat)
+        try:
+            mycursor.execute("CREATE TABLE feildrun_"+today+" (ID INT AUTO_INCREMENT PRIMARY KEY, PLANTNAME VARCHAR(255), SCORE VARCHAR(255), IMAGE VARCHAR(255), LON VARCHAR(255), LAT VARCHAR(255))")
+        except:
+            pass
+        mycursor.execute(sql, val)
+    else:
+        mydb = mysql.connector.connect(host="127.0.0.1", user="root", passwd="root", database="finalYearProject")
+        mycursor = mydb.cursor()
+        sql = "INSERT INTO feildrun_notdetected (PLANTNAME, SCORE, IMAGE, LON, LAT) VALUES (%s, %s, %s, %s, %s)"
+        val = ("", "", image, lon, lat)
+        try:
+            mycursor.execute("CREATE TABLE feildrun_notdetected (ID INT AUTO_INCREMENT PRIMARY KEY, PLANTNAME VARCHAR(255), SCORE VARCHAR(255), IMAGE VARCHAR(255), LON VARCHAR(255), LAT VARCHAR(255))")
+        except:
+            pass
+        mycursor.execute(sql, val)
+    mydb.commit()
+
+
+def sendThread():
+    global lock
+    HOST = '192.168.0.100'
+    while(1):
+        HEADERSIZE = 10
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((HOST, 12243))
+        #s.bind((socket.gethostname(), 12243))
+        s.listen(5)
+        if(len(dataToSend)>0):
+            #print(len(dataToSend))
+            clientsocket, address = s.accept()
+            #print(f"Connection from {address} has been established.")
+            msg = dataToSend[0]
+            msg = pickle.dumps(msg)
+            msg = bytes(f"{len(msg):<{HEADERSIZE}}", 'utf-8') + msg
+            #print(msg)
+            clientsocket.send(msg)
+            lock.acquire()
+            try:
+                del dataToSend[0]
+            finally:
+                lock.release()
+        else:
+            #time.sleep(.5)
+            pass
+
 
 def recieveThread():
     global lock
-    HOST = '192.168.0.171'
+    HOST = '192.168.0.102'
     while True:
         try:
             HEADERSIZE = 10
+
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            #s.connect((socket.gethostname(), 12243))
-            s.connect((HOST, 12243))
+            s.connect((HOST, 12345))
+            #s.connect((socket.gethostname(), 1243))
             full_msg = b''
             new_msg = True
             while True:
-                msg = s.recv(5)
+                msg = s.recv(4096)
                 if new_msg:
-                    print("new msg len:", msg[:HEADERSIZE])
+                    #print("new msg len:", msg[:HEADERSIZE])
                     msglen = int(msg[:HEADERSIZE])
                     new_msg = False
 
-                print("full message length:")
+                #print(f"full message length: {msglen}")
 
                 full_msg += msg
 
-                print(len(full_msg))
+                #print(len(full_msg))
 
                 if len(full_msg) - HEADERSIZE == msglen:
                     print("full msg recvd")
-                    print(full_msg[HEADERSIZE:])
+                    #print(full_msg[HEADERSIZE:])
                     d = pickle.loads(full_msg[HEADERSIZE:])
-                    print("GOT..................."+str(d.get("index")))
                     dataReceived.append(d)
                     new_msg = True
                     full_msg = b""
                     break
-
         except:
-            time.sleep(1)
+            #time.sleep(1)
+            pass
 
-def sendThread():
+
+def runDetect(dataToTest):
     global lock
-    HOST = '192.168.0.185'
-    while True:
-        HEADERSIZE = 10
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #s.bind((socket.gethostname(), 1243))
-        s.connect((HOST, 12243))
-        s.listen(5)
-        if len(dataToSend) > 0:
-            if dataToSend[0].get("index") not in detectedList:
-                clientsocket, address = s.accept()
-                print("Connection from "+address+" has been established.")
-                msg = dataToSend[0]
-                msg = pickle.dumps(msg)
-                msg = bytes('{:<10}'.format(len(msg)), 'utf-8') + msg
-                print(msg)
-                clientsocket.send(msg)
-                lock.acquire()
-                del dataToSend[0]
-                lock.release()
-            else:
-                lock.acquire()
-                del dataToSend[0]
-                lock.release()
-        else:
-            time.sleep(1)
-
-
-def addImage(image, index):
-    print("YOOOOOOOOOOOOOOOOOOOOO")
-    e = {}
-    cv2.imwrite('temp.jpg', image)
-    with open('temp.jpg', 'rb') as f:
-        Nimage = f.read()
-    e['image'] = Nimage
-    e['index'] = index
-    dataToSend.append(e)
-    os.remove("temp.jpg")
-
-
-def updatetime():
-    listsToDelete =[]
-    for x in range(len(myDictList)):
-        #print(len(myDictList))
-        if myDictList[x].get("time") > 60:
-            listsToDelete.insert(0, x)
-        else:
-            myDictList[x].update({"time": (myDictList[x].get("time") + 1)})
-            myDictList[x].update({"sendTime": (myDictList[x].get("sendTime") + 1)})
-    if len(listsToDelete) > 0:
-        for x in range(len(listsToDelete)):
-            del myDictList[listsToDelete[x]]
-
-
-def trackitem(loc_x, loc_y):
-    global trackCount
-    if len(myDictList) <= 0:
-        item = {"index": trackCount, "locX": loc_x, "locY": loc_y, "detected": 0, "time": 0, "sendTime": 0, "sprayed": 0}
-        myDictList.append(item)
-        trackCount += 1
-        return myDictList[0]
+    index = dataToTest.get("index")
+    if index in alreadyDetected:
+        pass
     else:
-        cIndex = None
-        cDiff = None
-        for x in range(len(myDictList)):
-            xDiff = abs(loc_x - myDictList[x].get("locX"))
-            yDIff = abs(loc_y - myDictList[x].get("locY"))
-            if (xDiff + yDIff) < 170:
-                if cDiff ==None:
-                    cDiff = xDiff + yDIff
-                    cIndex = x
-                elif (xDiff + yDIff) < cDiff:
-                    cDiff = xDiff + yDIff
-                    cIndex = x
-        if cIndex is None:
-            item = {"index": trackCount, "locX": loc_x, "locY": loc_y,
-                    "detected": 0, "time": 0, "sendTime": 0, "sprayed": 0}
-            myDictList.append(item)
-            trackCount += 1
-            return myDictList[len(myDictList) - 1]
+        print("in the detect")
+        doneIt = False
+        if len(doubleCheck) > 0:
+            for x in range(len(doubleCheck)):
+                if doubleCheck[x].get("index") == index:
+                    doneIt = True
         else:
-            myDictList[cIndex].update({"locX": loc_x})
-            myDictList[cIndex].update({"locY": loc_y})
-            myDictList[cIndex].update({"time": 0})
-            return myDictList[cIndex]
+            item = {"index": index, "mCheck": 0}
+            doubleCheck.append(item)
+        if not doneIt:
+            item = {"index": index, "mCheck": 0}
+            doubleCheck.append(item)
+            doneIt = True
+
+        image = dataToTest.get("image")
+        image = Image.open(io.BytesIO(image))
+        timeStamp = getTime();
+        image.save("img"+timeStamp+".jpg")
+
+        with open("img"+timeStamp+".jpg", 'rb') as file:
+            binaryData = file.read()
+
+        imageToRead = ("img" + "%s.jpg" % (timeStamp))
+        tensor = TensorImage(imageToRead)
+
+        #image3 = Image.open(io.BytesIO(tensor))
+        #cv2.imshow("fdsf",tensor)
+
+        inputOp = Ugraph.get_operation_by_name("import/" + inputLayer)
+        outputOp = Ugraph.get_operation_by_name("import/" + outputLayer)
+        with tf.Session(graph=Ugraph) as sess:
+            results = sess.run(outputOp.outputs[0], {
+                inputOp.outputs[0]: tensor
+            })
+
+        results = np.squeeze(results)
+        inOrderResults = results.argsort()[-1:][::-1]
+        gotDetect = False
+        for i in inOrderResults:
+            if (results[i] > .94):
+                gotDetect = True
+                newLoc = "C:/Users/shane/PycharmProjects/untitled/Detected/"+str(listOfNames[i])+"_"+\
+                         str(results[i])+"_"+timeStamp+".jpg"
+                os.rename("img" + "%s.jpg" % (timeStamp), newLoc)
+                print(results[i],listOfNames[i])
+                if doubleCheckEnabled:
+                    for q in range(len(doubleCheck)):
+                        if doubleCheck[q].get("index") == index:
+                            if listOfNames[i] == doubleCheck[q].get("mCheck"):
+                                #print("DFSHFHSFHGFHFHSHSG")
+                                getReadyForSend(newLoc,listOfNames[i],results[i],index,1,dataToTest.get("lon"),
+                                                dataToTest.get("lat"))
+                            else:
+                                doubleCheck[q].update({"mCheck": listOfNames[i]})
+                else:
+                    #print("DFSHFHSFHGFHFHSHSG")
+                    getReadyForSend(newLoc, listOfNames[i], results[i], index, 1, dataToTest.get("lon"),
+                                    dataToTest.get("lat"))
+        if not gotDetect:
+            newLoc = "C:/Users/shane/PycharmProjects/untitled/NotDetected/img" + str(timeStamp)+ ".jpg"
+            os.rename("img" + "%s.jpg" % (timeStamp), newLoc)
+            getReadyForSend(newLoc, "", "", index, 0,dataToTest.get("lon"),dataToTest.get("lat"))
+
+def TensorImage(inputImg):
+    fReader = tf.read_file(inputImg, "file_reader")
+    imgReader = tf.image.decode_jpeg(fReader, channels=3, name="jpeg_reader")
+    castFloat = tf.cast(imgReader, tf.float32)
+    expandDim = tf.expand_dims(castFloat, 0)
+    resized = tf.image.resize_bilinear(expandDim, [224, 224])
+    normalized = tf.divide(tf.subtract(resized, [0]), [255])
+    sess = tf.Session()
+    result = sess.run(normalized)
+    return result
 
 
-def checkForMatches(matches):
-    for q in range(len(myDictList)):
-        if myDictList[q].get("index") == matches.get("index"):
-            myDictList[q].update({"detected": 1})
+def getReadyForSend(image, name, score, index,detected,lon,lat):
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    if index not in alreadyDetected:
+        thread3 = threading.Thread(target=addToDatabase, args=(name,score,image,detected,lon,lat))
+        thread3.start()
+    if detected == 1:
+        r = {}
+        r['index'] = index
+        r['name'] = name
+        print("Sending "+name+ "Index: "+str(index))
+        dataToSend.append(r)
+        alreadyDetected.append(index)
 
 
-def spray(x, y):
-    print("Sprayed " + str(x) + " " + str(y))
+def GraphToMemory():
+  global Ugraph
+  Ugraph = tf.Graph()
+  graphDef = tf.GraphDef()
+  with open(TfModel, "rb") as m:
+    graphDef.ParseFromString(m.read())
+  with Ugraph.as_default():
+    tf.import_graph_def(graphDef)
+  #print("Im Here")
 
 
-def main():
+def GetNames(label_file):
+  label = []
+  proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
+  for l in proto_as_ascii_lines:
+    label.append(l.rstrip())
+  return label
+
+def getTime():
+    todaysDateTime = str(datetime.datetime.now())
+    time = todaysDateTime.split(" ")
+    calender = time[0].split("-")
+    year = calender[0]
+    month = calender[1]
+    day = calender[2]
+    clock = time[1].split(":")
+    hours = clock[0]
+    minuets = clock[1]
+    second = clock[2].split(".")
+    second = second[0]
+    dateTimeNow = str(day + "_" + month + "_" + year + "_" + hours + "_" + minuets + "_" + second)
+    return dateTimeNow
+
+
+def Main():
+    global lock
     thread = threading.Thread(target=sendThread)
     thread.start()
     thread2 = threading.Thread(target=recieveThread)
     thread2.start()
-    make_1080p
-    while True:
-        success, frame = cap.read()
-        frame = imutils.resize(frame, width=600)
-        dummy_frame = imutils.resize(frame, width=600)
-        blurred_frame = cv2.GaussianBlur(dummy_frame, (15, 15), 0)
-
-        # Convert BGR to HSV
-        hsv = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
-        sensitivity = 25
-
-        lower_green = np.array([50 - sensitivity, 50, 60])
-        upper_green = np.array([70 + sensitivity, 255, 255])
-        mask = cv2.inRange(hsv, lower_green, upper_green)  # I have the Green threshold image.
-
-        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        rects = []
-        for contour in contours:
-            area = cv2.contourArea(contour)
-            if area > 8000:
-                cv2.drawContours(dummy_frame, contour, -1, (0, 255, 0), 3)
-                rects.append(cv2.boundingRect(contour))
-                x, y, w, h = cv2.boundingRect(contour)
-                d = trackitem(round(x+(w*.5)), round(y+(h*.5)))
-                cv2.putText(dummy_frame, "Detected:"+str(d.get("index")),
-                            (round(x+(w*.5)), round(y+(h*.5))), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255))
-                if d.get("detected") == 0 and d.get("sendTime") > 60:
-                    for q in range(len(myDictList)):
-                        if myDictList[q].get("index") == d.get("index"):
-                            myDictList[q].update({"sendTime": 0})
-                    crop_img = frame[y:y + h, x:x + w]
-                    addImage(crop_img, d.get("index"))
-                elif d.get("detected") == 1 and d.get("sprayed") == 0:
-                    spray(d.get("locX"), d.get("locY"))
-                    for q in range(len(myDictList)):
-                        if myDictList[q].get("index") == d.get("index"):
-                            myDictList[q].update({"sprayed": 1})
-                            detectedList.append(myDictList[q].get("index"))
-
-        updatetime()
+    global listOfNames
+    listOfNames = GetNames(TfName)
+    GraphToMemory()
+    while(1):
         if len(dataReceived) > 0:
-            checkForMatches(dataReceived[0])
-            del dataReceived[0]
-
-        cv2.imshow('frame', frame)
-        cv2.imshow('dummyFrame', dummy_frame)
-        cv2.imshow('mask', mask)
-
-        # Esc to quit
-        if cv2.waitKey(1) & 0xFF == 27:
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
+            runDetect(dataReceived[0])
+            lock.acquire()
+            try:
+                del dataReceived[0]
+            finally:
+                lock.release()
+        else:
+            time.sleep(.3)
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    Main()
